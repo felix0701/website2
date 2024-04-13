f991a165aed8763a176a9471e8253dca9aa69a8e
ujjwal kumar <86791568+felix0701@users.noreply.github.com>
2024-04-13 16:45:07 +0530

    Delete scripts directory

--- scripts/fdatabase.py
+++
@@ -1,6 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-# Initialize the Firebase Admin SDK
-cred = credentials.Certificate("path/to/your/firebase-credentials.json")
-firebase_admin.initialize_app(cred)
